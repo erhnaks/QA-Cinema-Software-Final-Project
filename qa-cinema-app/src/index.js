2bed4c42ee293af5e065b599624c23f750f88641
erhnaks <erhnaks@hotmail.com>
2022-08-02 11:35:14 +0100

    removed gitignore file for clone purposes only

--- qa-cinema-app/src/index.js
+++ qa-cinema-app/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HomePage from './Components/HomePage/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <HomePage/>
   </React.StrictMode>
 );
 
