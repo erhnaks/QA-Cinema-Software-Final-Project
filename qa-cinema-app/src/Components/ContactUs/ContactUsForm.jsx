809900e9f2e8aff994bfe3e0264b9d613c8663d2
erhnaks <erhnaks@hotmail.com>
2022-08-03 15:22:54 +0100

    TF-15 #Contact us form has been created

--- qa-cinema-app/src/Components/ContactUs/ContactUsForm.jsx
+++ qa-cinema-app/src/Components/ContactUs/ContactUsForm.jsx
@@ -6,9 +6,49 @@ const ContactUsForm = () => {
   const [subject, setSubject] = useState("");
 
   return (
-    <Fragment>
-      <h1 className="contactUs"> Contact Us</h1>
-    </Fragment>
+    <>
+      <h1 className="contactUs"> Contact Us </h1>
+      <hr></hr>
+      <div className="form-div">
+        <form>
+          <br />
+
+          <from>
+            <label>Subject</label>
+            <input
+              className="contactUs-form"
+              type="text"
+              subject="Subject"
+              value={subject}
+              onChange={(e) => setSubject(e.target.value)}
+            />
+
+            <br />
+
+            <label className="contactUs-form-controller" size="large">
+              Details
+            </label>
+            <input
+              className="form-details"
+              type="text"
+              body="body"
+              value={body}
+              onChange={(e) => setBody(e.target.value)}
+            />
+            <br />
+
+            <div className="send">
+              <a
+                className="send"
+                href={`mailto:teamFoxtrot@example.com?subject=${subject}&body=${body}`}
+              >
+                Send
+              </a>
+            </div>
+          </from>
+        </form>
+      </div>
+    </>
   );
 };
 
