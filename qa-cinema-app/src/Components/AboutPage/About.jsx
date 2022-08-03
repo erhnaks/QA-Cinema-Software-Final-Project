43be6b686ba4e9d112f99314f8314fbb83d16209
MuhammadShayekurRahman <shayek.rahman@gmail.com>
2022-08-03 09:51:46 +0100

    TF-54 TF-27 TF-13  #Created about page

---
+++ qa-cinema-app/src/Components/AboutPage/About.jsx
@@ -0,0 +1,18 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Header from "../Header/Header";
+// import "./Home.css"
+
+const AboutPage = () => {
+return(
+<>
+   <Header></Header>
+   <Navbar></Navbar>
+   <h1>About</h1>  
+</>     
+)
+
+}
+
+export default AboutPage;
