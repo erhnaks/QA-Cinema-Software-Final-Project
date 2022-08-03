0fd64d3e2bc1500139702c570ae9db9fb14bbcef
MuhammadShayekurRahman <shayek.rahman@gmail.com>
2022-08-03 10:25:02 +0100

    TF-54 TF-27 TF-13  #Updated about page

--- qa-cinema-app/src/Components/AboutPage/About.jsx
+++ qa-cinema-app/src/Components/AboutPage/About.jsx
@@ -1,16 +1,44 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Header from "../Header/Header";
-// import "./Home.css"
+
 
 const AboutPage = () => {
 return(
 <>
    <Header></Header>
    <Navbar></Navbar>
-   <h1>About</h1>  
-</>     
+   <h1>About Us</h1>  
+
+  <div>
+      <figure class="text-center">
+        <blockquote class="blockquote">
+          <p>Take a seat in the clouds at QA Cinema Club, Join us now for an experience you will never forget...</p>
+        </blockquote>
+        <figcaption class="blockquote-footer">
+          Team <cite title="Source Title">Foxtrot</cite>
+        </figcaption>
+      </figure>
+  </div>   
+
+  <div>
+    {/* General Information about US */}
+
+  </div>
+
+  <div>
+    {/* Reviews */}
+  </div>  
+
+  <div>
+    {/* Location */}
+    International House, 1 St Katharine's Way, London E1W 1UN
+
+  </div> 
+
+</>  
+
 )
 
 }
