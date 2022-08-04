12f730f98521d6732e7d73477d35f845b5dcd630
erhnaks <erhnaks@hotmail.com>
2022-08-04 11:55:24 +0100

    TF-26 #Modal body has been created

--- qa-cinema-app/src/Front-End/Components/ListingsPage/ListingMovieDetails.jsx
+++ qa-cinema-app/src/Front-End/Components/ListingsPage/ListingMovieDetails.jsx
@@ -20,27 +20,40 @@ const ListingMovieDetails = ({
   const toggle= () => setModal(!modal);
 
 return (
-<React.Fragment>
+<>
 
 <CardLink className="btn btn-primary" onclick={toggle}> Movie Details</CardLink>
 <div className="modal-dialog modal-fullscreen-md-down">
   <Modal className="modal-container custom-modal-modal">
   id="listing-movie-details" dialogClassName="modal-dialog" isOpen={modal}>
     <ModalHeader id="modal-header">{title}</ModalHeader>
+<ModalBody id="modal-body">
+  <div className="col-md-12" id="modal-image">
+    <img src={poster} alt={title} width="30%" height="30%"/>
+  </div>
 
+                            <div className="col-md-12">
+                                <hr />
+                                <p id="director">Directed by {director}</p>
+                                <h6>The main cast: </h6>
+                                {
+                                    actors.map((actor) => (
+                                        <p id="cast">{actor}</p>
+                                    ))
+                                }
+                                <br />
+                                <p id="runtime">Runtime: {runtime}</p>
+                                <p id="card-rating">Rating: {rating}</p>
+                            </div>
+                        </div>
+                        </ModalBody>
+                    <ModalFooter id="modalfooter">
+                        <button onClick={toggle} className="btn btn-outline-danger">Go Back</button>
+                    </ModalFooter>
+                </Modal>
+            </div>
 
-
-
-
-
-    </Modal>
-</div>
-
-
-
-
-
-</React.Fragment>
+</>
 
 
 
