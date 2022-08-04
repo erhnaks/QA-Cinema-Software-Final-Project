d96b691be7c3f9fbd595d4cf2d1db728166b1291
erhnaks <erhnaks@hotmail.com>
2022-08-04 11:57:01 +0100

    Bug has been found and fixed the closing tag bug

--- qa-cinema-app/src/Front-End/Components/ListingsPage/ListingMovieDetails.jsx
+++ qa-cinema-app/src/Front-End/Components/ListingsPage/ListingMovieDetails.jsx
@@ -17,47 +17,48 @@ const ListingMovieDetails = ({
   actors,
 }) => {
   const [modal, setModal] = useState(false);
-  const toggle= () => setModal(!modal);
+  const toggle = () => setModal(!modal);
 
-return (
-<>
+  return (
+    <>
+      <CardLink className="btn btn-primary" onclick={toggle}>
+        {" "}
+        Movie Details
+      </CardLink>
+      <div className="modal-dialog modal-fullscreen-md-down">
+        <Modal
+          className="modal-container custom-modal-modal"
+          id="listing-movie-details"
+          dialogClassName="modal-dialog"
+          isOpen={modal}
+        >
+          <ModalHeader id="modal-header">{title}</ModalHeader>
+          <ModalBody id="modal-body">
+            <div class="row-md">
+              <div className="col-md-12" id="modal-image">
+                <img src={poster} alt={title} width="30%" height="30%" />
+              </div>
 
-<CardLink className="btn btn-primary" onclick={toggle}> Movie Details</CardLink>
-<div className="modal-dialog modal-fullscreen-md-down">
-  <Modal className="modal-container custom-modal-modal">
-  id="listing-movie-details" dialogClassName="modal-dialog" isOpen={modal}>
-    <ModalHeader id="modal-header">{title}</ModalHeader>
-<ModalBody id="modal-body">
-  <div className="col-md-12" id="modal-image">
-    <img src={poster} alt={title} width="30%" height="30%"/>
-  </div>
-
-                            <div className="col-md-12">
-                                <hr />
-                                <p id="director">Directed by {director}</p>
-                                <h6>The main cast: </h6>
-                                {
-                                    actors.map((actor) => (
-                                        <p id="cast">{actor}</p>
-                                    ))
-                                }
-                                <br />
-                                <p id="runtime">Runtime: {runtime}</p>
-                                <p id="card-rating">Rating: {rating}</p>
-                            </div>
-                        </div>
-                        </ModalBody>
-                    <ModalFooter id="modalfooter">
-                        <button onClick={toggle} className="btn btn-outline-danger">Go Back</button>
-                    </ModalFooter>
-                </Modal>
+              <div className="col-md-12">
+                <hr />
+                <p id="director">Directed by {director}</p>
+                <h6>The main cast: </h6>
+                {actors.map((actor) => (
+                  <p id="cast">{actor}</p>
+                ))}
+                <br />
+                <p id="runtime">Runtime: {runtime}</p>
+                <p id="card-rating">Rating: {rating}</p>
+              </div>
             </div>
-
-</>
-
-
-
-)
-
-
+          </ModalBody>
+          <ModalFooter id="modalfooter">
+            <button onClick={toggle} className="btn btn-outline-danger">
+              Go Back
+            </button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    </>
+  );
 };
