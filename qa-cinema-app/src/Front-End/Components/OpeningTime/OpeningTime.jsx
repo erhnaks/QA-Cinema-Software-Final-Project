d56d1c25f090c3985097496d10eda09abb6a5b93
shakir-ali-Github <shakirali_@hotmail.co.uk>
2022-08-05 16:35:45 +0100

    added timings

--- qa-cinema-app/src/Front-End/Components/OpeningTime/OpeningTime.jsx
+++ qa-cinema-app/src/Front-End/Components/OpeningTime/OpeningTime.jsx
@@ -6,15 +6,38 @@ const OpeningTime = () => {
   return (
     <>
     <Header></Header>
+
+    <div className="container">
+    <h1>Opening Times</h1>
+    </div>
+
+    <div className="row d-flex justify-content-around">
+          <div className="card col-6 p-3 drop-shadow">
+            {/* General Information about US */}
+            <h3>Our Opening times are:</h3>
+            Monday:    08:00 AM - 11:30 PM
+            <br></br>
+            Tuesday:   08:00 AM - 11:30 PM
+            <br></br>
+            Wednesday: 08:00 AM - 11:30 PM
+            <br></br>
+            Thursday:  08:00 AM - 11:30 PM
+            <br></br>
+            Friday:    08:00 AM - 11:30 PM
+            <br></br>
+            Saturday:  08:00 AM - 01:00 AM
+            <br></br>
+            Sunday:    09:00 AM - 11:00 PM
+          </div>
+    </div>
+
+
+
+
+
+
     <Footer></Footer>
-    
-    
-    
-    
     </>
-
-
-
   );
   
 };
