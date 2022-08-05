cc7e71ee6843a1c4fbf503d5c538bb700c0c68ed
MuhammadShayekurRahman <shayek.rahman@gmail.com>
2022-08-05 16:06:36 +0100

    TF-114 TF-115 #Added Screen pictures

--- qa-cinema-app/src/Front-End/Components/ScreensPage/Screens.jsx
+++ qa-cinema-app/src/Front-End/Components/ScreensPage/Screens.jsx
@@ -1,15 +1,34 @@
 import React from 'react';
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import screenStandard from "../../../Resources/images/Seats_Standard.jpg";
+import screenLuxe from "../../../Resources/images/Seats_Luxe.jpg";
 
 const Screens = () => {
 
     return (
         <>
             <Header></Header>
-            <div className='container fluid'>
+            <div className='container fluid p-3'>
                 <h1>Screens</h1>
+                <div className='row'>
+                    <h3>Standard Screen</h3>
+                    <div className="col-6">
 
+                    </div>
+                    <div className="col-6">
+                        <img src={screenStandard} alt='screen standard' width="100%"/>                   
+                    </div>
+                </div>
+                <div className='row'>
+                    <h3>Luxe Screen</h3>
+                    <div className="col-6">
+
+                    </div>
+                    <div className="col-6">
+                        <img src={screenLuxe} alt='screen standard' width="100%"/>                   
+                    </div>
+                </div>
             </div>
             <Footer></Footer>
         </>
