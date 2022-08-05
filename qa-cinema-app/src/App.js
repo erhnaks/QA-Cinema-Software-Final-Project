f3fafc0e8746e4099bfee9e208263bc4a1aa8e01
MuhammadShayekurRahman <shayek.rahman@gmail.com>
2022-08-05 15:37:14 +0100

    TF-114 TF-115 #Created Screens Page

--- qa-cinema-app/src/App.js
+++ qa-cinema-app/src/App.js
@@ -5,6 +5,7 @@ import HomePage from "./Front-End/Components/HomePage/Home";
 import AboutPage from "./Front-End/Components/AboutPage/About";
 import AllListings from "./Front-End/Components/ListingsPage/AllListings";
 import NewReleases from "./Front-End/Components/NewReleases/NewReleases";
+import Screens from "./Front-End/Components/ScreensPage/Screens";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/contactUs" element={<ContactUs />} />
           <Route path="/listings" element={<AllListings />} />
           <Route path="/comingsoon" element={<NewReleases />} />
+          <Route path="/screens" element={<Screens />} />
         </Routes>
       </Router>
     </div>
---
+++ qa-cinema-app/src/Front-End/Components/ScreensPage/Screens.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+
+const Screens = () => {
+
+    return (
+        <>
+            <Header></Header>
+            <div className='container fluid'>
+                <h1>Screens</h1>
+
+            </div>
+            <Footer></Footer>
+        </>
+
+    )
+
+
+
+}
+
+export default Screens;
