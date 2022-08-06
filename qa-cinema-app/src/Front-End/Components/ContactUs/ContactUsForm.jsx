4994244ce4b435db3142ba67c6e3f27281e597be
erhnaks <erhnaks@hotmail.com>
2022-08-06 13:43:27 +0100

    TF-116 new custom form has been created

--- qa-cinema-app/src/Front-End/Components/ContactUs/ContactUsForm.jsx
+++ qa-cinema-app/src/Front-End/Components/ContactUs/ContactUsForm.jsx
@@ -112,16 +112,63 @@ export default class ContactUsForm extends Component {
       <div className="container">
         <form>
           {/* Single name item */}
-          <div className="singleItem"></div>
-
-          <label htmlFor="name">Name: </label>
-          <input
-            type="text"
-            name="name"
-            placeholder="Your name..."
-            className="name"
-          />
+          <div className="singleItem">
+            <label htmlFor="name">Name: </label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Your name..."
+              className="name"
+            />
+          </div>
           {/* End of name item */}
+          {/* Single surname item */}
+          <div className="singleItem">
+            <label htmlFor="surname">Surname: </label>
+            <input
+              type="text"
+              name="surname"
+              placeholder="Your surname..."
+              className="name"
+            />
+          </div>
+          {/* End of surname item */}
+
+          {/* Single email item */}
+          <div className="singleItem">
+            <label htmlFor="email">E-mail: </label>
+            <input
+              type="text"
+              name="Email"
+              placeholder="Your e-mail..."
+              className="Email"
+            />
+          </div>
+          {/* End of email item */}
+
+          {/* Single message item */}
+
+          <div className="singleItem">
+            <label htmlFor="message">Message: </label>
+            <textarea
+              name="message"
+              placeholder="Your message..."
+              id=""
+              cols="30"
+              rows="10"
+            ></textarea>
+          </div>
+          {/* End of message item */}
+
+          <div className="msg">
+            Your message has been sent to our customer service department. One
+            of our team member will get back to you as soon as possible.
+          </div>
+
+          <div className="btn"></div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
         </form>
       </div>
     );
