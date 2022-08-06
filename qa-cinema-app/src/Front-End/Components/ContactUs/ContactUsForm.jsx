1c1dc985e0e30c0739dc4c61e3c9fd7c3869b16b
erhnaks <erhnaks@hotmail.com>
2022-08-06 13:30:24 +0100

    TF-116 Buildigin contact  section with Nodemailer

--- qa-cinema-app/src/Front-End/Components/ContactUs/ContactUsForm.jsx
+++ qa-cinema-app/src/Front-End/Components/ContactUs/ContactUsForm.jsx
@@ -72,34 +72,58 @@
 // Valid Email Format: Given the user’s email is not in standard email address format when the Submit
 // button is clicked, then the user gets a message indicating the email must be in a valid format.
 
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import "./ContactUs.css";
+// import Form from "react-bootstrap/Form";
+// import Button from "react-bootstrap/Button";
+// import "./ContactUs.css";
 
-const ContactUsForm = () => {
-  return (
-    <>
-      <Form>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Full Name</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Please enter your full name"
+// const ContactUsForm = () => {
+//   return (
+//     <>
+//       <Form>
+//         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+//           <Form.Label>Full Name</Form.Label>
+//           <Form.Control
+//             type="email"
+//             placeholder="Please enter your full name"
+//           />
+//           <Form.Label>Email address</Form.Label>
+//           <Form.Control type="email" placeholder="name@example.com" />
+//         </Form.Group>
+//         <Form.Label>Subject</Form.Label>
+//         <Form.Control type="email" placeholder="Subject" />
+//         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+//           <Form.Label>Message:</Form.Label>
+//           <Form.Control as="textarea" rows={5} />
+//         </Form.Group>
+//       </Form>
+//       <br></br>
+//       <Button type="submit">Send</Button>
+//     </>
+//   );
+// };
+
+// export default ContactUsForm;
+
+import React, { Component } from "react";
+
+export default class ContactUsForm extends Component {
+  render() {
+    return (
+      <div className="container">
+        <form>
+          {/* Single name item */}
+          <div className="singleItem"></div>
+
+          <label htmlFor="name">Name: </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your name..."
+            className="name"
           />
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="name@example.com" />
-        </Form.Group>
-        <Form.Label>Subject</Form.Label>
-        <Form.Control type="email" placeholder="Subject" />
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Message:</Form.Label>
-          <Form.Control as="textarea" rows={5} />
-        </Form.Group>
-      </Form>
-      <br></br>
-      <Button type="submit">Send</Button>
-    </>
-  );
-};
-
-export default ContactUsForm;
+          {/* End of name item */}
+        </form>
+      </div>
+    );
+  }
+}
