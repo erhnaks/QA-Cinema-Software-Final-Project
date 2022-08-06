2c8271b1bb1c03b672ed3f17ba8d1279b4f8f611
erhnaks <erhnaks@hotmail.com>
2022-08-06 18:53:36 +0100

    TF-116 New contactUs form has been created

--- qa-cinema-app/src/Front-End/Components/ContactUs/ContactUsForm.jsx
+++ qa-cinema-app/src/Front-End/Components/ContactUs/ContactUsForm.jsx
@@ -1,59 +1,159 @@
-import React, { Fragment } from "react";
-import { useState } from "react";
-import "./ContactUs.css";
+import React, { Component } from "react";
+import axios from "axios";
 
-const ContactUsForm = () => {
-  const [body, setBody] = useState("");
-  const [subject, setSubject] = useState("");
+export default class ContactUsForm extends Component {
+  state = {
+    name: "",
+    surname: "",
+    email: "",
+    message: "",
+    sent: false,
+  };
 
-  return (
-    <>
-      <h1
-        className="contact-h1
-"
-      >
-        {" "}
-        Contact Us{" "}
-      </h1>
-      <hr></hr>
-      <div className="form-div">
-        <form>
-          <br />
+  // handle inputs
 
-          <from>
-            <label>Subject: </label>
+  handleName = (e) => {
+    this.setState({ name: e.target.value });
+  };
+
+  handleSurname = (e) => {
+    this.setState({ surname: e.target.value });
+  };
+
+  handleEmail = (e) => {
+    this.setState({ email: e.target.value });
+  };
+
+  handleMessage = (e) => {
+    this.setState({ message: e.target.value });
+  };
+
+  // End of handle inputs
+
+  // Form Submit function
+
+  formSubmit = (e) => {
+    e.preventDefault();
+
+    let data = {
+      name: this.state.name,
+      surname: this.state.surname,
+      email: this.state.email,
+      message: this.state.message,
+    };
+
+    // Axios Post methods
+
+    axios
+      .post("/api/form", data)
+      .then((res) => {
+        this.setState(
+          {
+            sent: true,
+          },
+          this.resetForm()
+        );
+      })
+      .catch(() => {
+        console.error("Oppps, Something went wrong. Please try again later");
+      });
+  };
+
+  // End of Form Submit function
+
+  // Resetting Form function
+
+  resetForm = () => {
+    this.setState({
+      name: "",
+      surname: "",
+      email: "",
+      message: "",
+    });
+
+    setTimeout(() => {
+      this.setState({
+        sent: false,
+      });
+    }, 3000);
+  };
+
+  //End of Form Resetting
+
+  render() {
+    return (
+      <div className="container">
+        <form onClick={this.formSubmit}>
+          {/* Single name item */}
+          <div className="singleItem">
+            <label htmlFor="name">Name: </label>
             <input
               type="text"
-              subject="Subject"
-              value={subject}
-              onChange={(e) => setSubject(e.target.value)}
+              name="name"
+              placeholder="Your name..."
+              className="name"
+              value={this.state.name}
+              onChange={this.handleName}
             />
-
-            <br />
-
-            <label>Details:</label>
-            <textarea
-              className="form-details"
+          </div>
+          {/* End of name item */}
+          {/* Single surname item */}
+          <div className="singleItem">
+            <label htmlFor="surname">Surname: </label>
+            <input
               type="text"
-              body="body"
-              value={body}
-              onChange={(e) => setBody(e.target.value)}
-            ></textarea>
-            <br />
+              name="surname"
+              placeholder="Your surname..."
+              className="surname"
+              value={this.state.surname}
+              onChange={this.handleSurname}
+            />
+          </div>
+          {/* End of surname item */}
 
-            <div className="send">
-              <a
-                className="send"
-                href={`mailto:teamFoxtrot@qa-cinema.com?subject=${subject}&body=${body}`}
-              >
-                Send
-              </a>
-            </div>
-          </from>
+          {/* Single email item */}
+          <div className="singleItem">
+            <label htmlFor="email">E-mail: </label>
+            <input
+              type="text"
+              name="Email"
+              placeholder="Your e-mail..."
+              className="Email"
+              value={this.state.email}
+              onChange={this.handleEmail}
+              required
+            />
+          </div>
+          {/* End of email item */}
+
+          {/* Single message item */}
+
+          <div className="singleItem">
+            <label htmlFor="message">Message: </label>
+            <textarea
+              name="message"
+              placeholder="Your message..."
+              id=""
+              cols="30"
+              rows="10"
+              value={this.state.message}
+              onChange={this.handleMessage}
+              required
+            ></textarea>
+          </div>
+          {/* End of message item */}
+          <hr></hr>
+          <div className={this.state.sent ? "msg msgAppear" : "msg"}>
+            Your message has been sent to our customer service department. One
+            of our team member will get back to you as soon as possible.
+          </div>
+          <hr></hr>
+
+          <div className="btn-form">
+            <button type="submit">Submit</button>
+          </div>
         </form>
       </div>
-    </>
-  );
-};
-
-export default ContactUsForm;
+    );
+  }
+}
