3eca3fa71c07b0b93da6db103c33dfbf1e95a77f
shakir-ali-Github <shakirali_@hotmail.co.uk>
2022-08-08 14:19:18 +0100

    added image into opening times page

--- qa-cinema-app/src/Front-End/Components/OpeningTime/OpeningTime.jsx
+++ qa-cinema-app/src/Front-End/Components/OpeningTime/OpeningTime.jsx
@@ -13,7 +13,7 @@ const OpeningTime = () => {
 
     <div className="row d-flex justify-content-around">
           <div className="card col-6 p-3 drop-shadow">
-            {/* General Information about US */}
+            {/* Actual opening timings */}
             <h3>Our Opening times are:</h3>
             Monday:    08:00 AM - 11:30 PM
             <br></br>
@@ -28,6 +28,14 @@ const OpeningTime = () => {
             Saturday:  08:00 AM - 01:00 AM
             <br></br>
             Sunday:    09:00 AM - 11:00 PM
+          </div>
+
+         
+          <div className="card col-4 p-3 drop-shadow">
+            {/* Image  */}
+            <h3>What are you waiting for? Visit Us Today</h3>
+            International House, 1 St Katharine's Way, London E1W 1UN
+            <img src= "https://images.pexels.com/photos/3537844/pexels-photo-3537844.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"></img>
           </div>
     </div>
 
