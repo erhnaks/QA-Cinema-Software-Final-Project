93475c2a50d34873aa64a87b1de491faf9cb85f9
shakir-ali-Github <shakirali_@hotmail.co.uk>
2022-08-08 15:32:09 +0100

    modified opening times page

--- qa-cinema-app/src/Front-End/Components/OpeningTime/OpeningTime.jsx
+++ qa-cinema-app/src/Front-End/Components/OpeningTime/OpeningTime.jsx
@@ -7,6 +7,8 @@ const OpeningTime = () => {
     <>
     <Header></Header>
 
+    <br></br>
+
     <div className="container">
     <h1 class="text-center">"I'm the type of person who eats all the popcorn while they're still going through the trailers"</h1>
     </div>
@@ -42,10 +44,8 @@ const OpeningTime = () => {
             <img src= "https://images.pexels.com/photos/3537844/pexels-photo-3537844.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"></img>
           </div>
     </div>
-
-
-
-
+    <br></br>
+    <br></br>
 
 
     <Footer></Footer>
