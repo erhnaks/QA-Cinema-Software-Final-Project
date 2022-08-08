0908734bfa6a31db26da8162b3b405c4d3539771
shakir-ali-Github <shakirali_@hotmail.co.uk>
2022-08-08 15:23:45 +0100

    TF-111 TF-112 #complete opening times

--- qa-cinema-app/src/Front-End/Components/OpeningTime/OpeningTime.jsx
+++ qa-cinema-app/src/Front-End/Components/OpeningTime/OpeningTime.jsx
@@ -8,33 +8,37 @@ const OpeningTime = () => {
     <Header></Header>
 
     <div className="container">
-    <h1>"I'm the type of person who eats all the popcorn while they're still going through the trailers" - Everyone</h1>
+    <h1 class="text-center">"I'm the type of person who eats all the popcorn while they're still going through the trailers"</h1>
     </div>
+    <br></br>
 
     <div className="row d-flex justify-content-around">
           <div className="card col-6 p-3 drop-shadow">
             {/* Actual opening timings */}
-            <h3>Our Opening times are:</h3>
+            <h2 class="text-center">Our Opening times are:</h2>
+            <hr />
+            <h5 class="text-center">
             Monday:    08:00 AM - 11:30 PM
-            <br></br>
+            <br></br><br></br>
             Tuesday:   08:00 AM - 11:30 PM
-            <br></br>
+            <br></br><br></br>
             Wednesday: 08:00 AM - 11:30 PM
-            <br></br>
+            <br></br><br></br>
             Thursday:  08:00 AM - 11:30 PM
-            <br></br>
+            <br></br><br></br>
             Friday:    08:00 AM - 11:30 PM
-            <br></br>
+            <br></br><br></br>
             Saturday:  08:00 AM - 01:00 AM
-            <br></br>
+            <br></br><br></br>
             Sunday:    09:00 AM - 11:00 PM
+            </h5>
           </div>
 
          
           <div className="card col-4 p-3 drop-shadow">
             {/* Image  */}
-            <h3>What are you waiting for? Visit Us Today</h3>
-            International House, 1 St Katharine's Way, London E1W 1UN
+            <h3 class="text-center">What are you waiting for?<br></br>Visit Us Today</h3>
+            <h8 class="text-center">International House, 1 St Katharine's Way, London E1W 1UN</h8>
             <img src= "https://images.pexels.com/photos/3537844/pexels-photo-3537844.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"></img>
           </div>
     </div>
