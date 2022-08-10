d0384ddb6297413a5ac749d59a81c1d9083b5593
erhnaks <erhnaks@hotmail.com>
2022-08-10 14:51:59 +0100

    Working on styling css for search

--- qa-cinema-app/src/Front-End/Components/Search/Search.jsx
+++ qa-cinema-app/src/Front-End/Components/Search/Search.jsx
@@ -1,49 +1,61 @@
-import React, {useState} from "react";
-import '../Search/SearchStyle.css';
+import React, { useState } from "react";
+import "../Search/SearchStyle.css";
 import data from "./SearchData.json";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
 
 function Search() {
   const [searchTerm, setSearchTerm] = useState("");
   return (
     <>
+      <Header></Header>
+
       <div className="templateContainer">
         <div className="searchInput_Container">
-          <input id="searchInput" type="text" placeholder="Search here for movies..." onChange={(event) => {
-            setSearchTerm(event.target.value);
-          }} />
+          <input
+            id="searchInput"
+            type="text"
+            placeholder="Search here for movies..."
+            onChange={(event) => {
+              setSearchTerm(event.target.value);
+            }}
+          />
         </div>
         <div className="template_Container">
-          {
-            data 
-              .filter((val) => {
-                if(searchTerm == ""){
-                  return val;
-                }else if(val.title.toLowerCase().includes(searchTerm.toLowerCase())){
-                  return val;
-                }
-              })
-              .map((val) => {
-                return(
-                  <div className="template" key={val.img}>
-                      <img src={val.img} alt="" />
-                      <h3>{val.title}</h3>
-                      <hr></hr>
-                      <p className="description">{val.description}</p>
-                      <hr></hr>
-                      <p>Actors :</p>
-                      <p className="actors">{val.actors}</p>
-                      <hr></hr>
-                      <p className="director">Director : {val.director}</p>
-                      <hr></hr>
-                      <p className="rating">Rating : {val.rating}</p>
-                  </div> 
-                )
-              })
-          }
+          {data
+            .filter((val) => {
+              if (searchTerm == "") {
+                return val;
+              } else if (
+                val.title.toLowerCase().includes(searchTerm.toLowerCase())
+              ) {
+                return val;
+              }
+            })
+            .map((val) => {
+              return (
+                <div className="template" key={val.img}>
+                  <img src={val.img} alt="" />
+                  <h3>{val.title}</h3>
+                  <hr></hr>
+                  <p className="description">{val.description}</p>
+                  <hr></hr>
+                  <p>Actors :</p>
+                  <p className="actors">{val.actors}</p>
+                  <hr></hr>
+                  <p className="director">Director : {val.director}</p>
+                  <hr></hr>
+                  <p className="runtime">Runtime : {val.runtime}</p>
+                  <hr></hr>
+                  <p className="rating">Rating : {val.rating}</p>
+                </div>
+              );
+            })}
         </div>
       </div>
+      <Footer></Footer>
     </>
-  )
+  );
 }
 
 export default Search;
