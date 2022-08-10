6fd45dc51dd19871fef85d004e3f11b428a88b0e
erhnaks <erhnaks@hotmail.com>
2022-08-10 21:29:37 +0100

    TF-118 Create method has been implemented

--- qa-cinema-app/src/Front-End/Components/Comments/Controllers/PostCommentController.js
+++ qa-cinema-app/src/Front-End/Components/Comments/Controllers/PostCommentController.js
@@ -1,5 +1,6 @@
 const express = require("express");
 var router = express();
+var ObjectID = require("mongoose").Types.ObjectId;
 
 var { PostComment } = require("../models/PostComment");
 
@@ -12,6 +13,26 @@ router.get("/", (req, res) => {
           JSON.stringify(err, undefined, 2)
       );
   });
+
+  router.post("/", (req, res) => {
+    var newRecord = new PostMessage({
+      title: req.body.title,
+      name: req.body.name,
+      message: req.body.message,
+      rating: req.body.rating,
+    });
+
+    newRecord.save((err, docs) => {
+      if (!err) res.send(docs);
+      else
+        console.log(
+          "Error while creating new record : " +
+            JSON.stringify(err, undefined, 2)
+        );
+    });
+  });
+
+
 });
 
 module.exports = router;
