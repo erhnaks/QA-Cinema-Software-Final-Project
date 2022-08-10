aa237fc09f5383817946f204730ca24cf98c478a
erhnaks <erhnaks@hotmail.com>
2022-08-10 22:00:56 +0100

    TF-118 CRUD created and tested with POSTMAN

--- qa-cinema-app/src/Front-End/Components/Comments/Controllers/PostCommentController.js
+++ qa-cinema-app/src/Front-End/Components/Comments/Controllers/PostCommentController.js
@@ -15,7 +15,7 @@ router.get("/", (req, res) => {
   });
 
   router.post("/", (req, res) => {
-    var newRecord = new PostMessage({
+    var newRecord = new PostComment({
       title: req.body.title,
       name: req.body.name,
       message: req.body.message,
@@ -28,6 +28,45 @@ router.get("/", (req, res) => {
         console.log(
           "Error while creating new record : " +
             JSON.stringify(err, undefined, 2)
+        );
+    });
+  });
+
+  router.put("/:id", (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+      return res.status(400).send("No record with given id : " + req.params.id);
+
+    var updatedRecord = {
+      title: req.body.title,
+      name: req.body.name,
+      message: req.body.message,
+      rating: req.body.rating,
+    };
+
+    PostComment.findByIdAndUpdate(
+      req.params.id,
+      { $set: updatedRecord },
+      { new: true },
+      (err, docs) => {
+        if (!err) res.send(docs);
+        else
+          console.log(
+            "Error while updating a record : " +
+              JSON.stringify(err, undefined, 2)
+          );
+      }
+    );
+  });
+
+  router.delete("/:id", (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+      return res.status(400).send("No record with given id : " + req.params.id);
+
+    PostComment.findByIdAndRemove(req.params.id, (err, docs) => {
+      if (!err) res.send(docs);
+      else
+        console.log(
+          "Error while deleting a record : " + JSON.stringify(err, undefined, 2)
         );
     });
   });
