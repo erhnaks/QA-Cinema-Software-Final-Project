a25ac81b8c2681d0a5fa8b5d2f7e8ac3d44d4831
erhnaks <erhnaks@hotmail.com>
2022-08-11 11:03:56 +0100

    modified app.js

--- qa-cinema-app/src/App.js
+++ qa-cinema-app/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import ContactUs from "./Front-End/Components/ContactUs/ContactUsPage";
+
 import HomePage from "./Front-End/Components/HomePage/Home";
 import AboutPage from "./Front-End/Components/AboutPage/About";
 import Booking from "./Front-End/Components/BookingPage/Booking";
@@ -23,7 +23,6 @@ function App() {
         <Routes>
           <Route exact path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
-          <Route path="/contactUs" element={<ContactUs />} />
           <Route path="/booking" element={<Booking />} />
           <Route path="/listings" element={<AllListings />} />
           <Route path="/places" element={<Places />} />
