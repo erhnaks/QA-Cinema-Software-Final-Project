8aad0020d6a168b9dbcea4ad931de444fe4a2651
erhnaks <erhnaks@hotmail.com>
2022-08-11 11:30:10 +0100

    fixed warnings

--- qa-cinema-app/src/index.js
+++ qa-cinema-app/src/index.js
@@ -7,9 +7,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <React.Fragment>
     <App />
-  </React.StrictMode>
+  </React.Fragment>
 );
 
 reportWebVitals();
